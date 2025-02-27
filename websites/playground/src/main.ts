dd9bdc95f8ed8b25fe265c6b11f405c411a05bc7
g30021214 <guokainan@huawei.com>
2025-02-27 20:55:29 +0800

    add websites

---
+++ websites/playground/src/main.ts
@@ -0,0 +1,100 @@
+/* eslint-disable no-alert */
+import { createEditor, createViewer } from '@v-md/app'
+import { editorVolarPlugin } from '@v-md/plugin-editor-volar'
+// 产物测试时需要手动引入样式
+// import '@v-md/app/dist/assets/style.css'
+
+const url = new URL(location.href)
+const {
+  origin,
+  pathname,
+  hash,
+  search,
+} = url
+
+const noFilesNav = /files=false/.test(search)
+
+if (pathname.endsWith('/view') || pathname.endsWith('/view/')) {
+  createViewer('#app', {
+    cdnUrl: 'https://unpkg.openx.huawei.com',
+    value: hash.slice(1),
+  })
+}
+else {
+  const editor = createEditor({
+    attrs: {
+      style: {
+        height: '100vh',
+      },
+      navVisible: !noFilesNav,
+    },
+    cdnUrl: 'https://unpkg.openx.huawei.com',
+    cdnType: 'unpkg',
+    value: hash.slice(1),
+    plugins: (editor) => {
+      editor.use(editorVolarPlugin({
+        vueWorker: async () => {
+          const { default: VueWorker } = await import('@v-md/plugin-editor-volar/vue.worker?worker')
+          return new VueWorker()
+        },
+        editorWorker: async () => {
+          const { default: EditorWorker } = await import('monaco-editor-core/esm/vs/editor/editor.worker?worker')
+          return new EditorWorker()
+        },
+      }))
+    },
+    toolbars: {
+      preview: {
+        items: [
+          {
+            name: 'jump',
+            type: 'button',
+            icon: import('./assets/eye.svg').then(m => m.default),
+            tip: '查看预览效果',
+            onTrigger: (app) => {
+              window.open(`${origin}${pathname}/view#${app.files.compiledValue.value}`, '_blank')
+            },
+          },
+          {
+            name: 'copy-edit-code',
+            type: 'button',
+            icon: import('./assets/copy.svg').then(m => m.default),
+            tip: '复制预览码',
+            onTrigger: (app) => {
+              navigator.clipboard.writeText(app.files.compiledValue.value).then(() => {
+                alert('复制成功')
+              }).catch(() => {
+                alert('复制失败')
+              })
+            },
+          },
+          {
+            name: 'copy-view-code',
+            type: 'button',
+            icon: import('./assets/copy.svg').then(m => m.default),
+            tip: '复制编辑码',
+            onTrigger: (app) => {
+              navigator.clipboard.writeText(app.files.value.value).then(() => {
+                alert('复制成功')
+              }).catch(() => {
+                alert('复制失败')
+              })
+            },
+          },
+          {
+            name: 'files-nav',
+            type: 'switch',
+            icon: import('./assets/folder-cancel.svg').then(m => m.default),
+            tip: '禁用文件导航',
+            value: noFilesNav,
+            onTrigger: (app, value) => {
+              app.options.attrs.navVisible = !value
+            },
+          },
+        ],
+      },
+    },
+  })
+
+  editor.mount('#app')
+}
